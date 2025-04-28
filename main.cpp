727c3a3a82a7c353d7f05c50bbdb030546bd9336
Jakub Czechowski <kubaczech911@gmail.com>
2025-04-28 14:06:16 +0200

    compatibility difficulties resolved

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include "utilities.h"
 #include <chrono>
@@ -8,7 +7,7 @@ int main() {
     auto start = std::chrono::high_resolution_clock::now();
 
     // LinkedList list = LinkedList();
-    
+
     // Initializing a stack
     Stack stacky;
 
@@ -20,8 +19,8 @@ int main() {
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
     std::cout << "Stack's top element: " << stacky.top() << std::endl;
-    std::cout << "Time duration of pushing " << stacky.stackSize << " integers onto the stack: " 
-              << elapsed.count() << " seconds. " << std::endl;
+    std::cout << "Time duration of pushing " << stacky.stackSize << " integers onto the stack: "
+        << elapsed.count() << " seconds. " << std::endl;
 
     std::cout << "Try to push an int: " << std::endl;
     std::cin >> add;
@@ -31,21 +30,4 @@ int main() {
     std::cout << "Checking if the stack is full: " << stacky.isFull() << std::endl;
 
     return 0;
-=======
-#include <iostream>
-#include "utilities.h"
-
-int main() {
-    
-    LinkedList list = LinkedList();
-    list.insertRight(10);
-    list.insertRight(20);
-    list.insertLeft(-10);
-    list.display();
-    list.eraseLeft();
-    list.eraseRight();
-    list.display();
-    std::cout << std::endl << list.getSize();
-    return 0;
->>>>>>> d2e3841134cb1defd9e103e029e7f87d96285655
 }
